e6381ccea389cc3c1704e447b88b7f4bbcc3341d
Cheng Pin <as48957210@gmail.com>
2023-07-03 09:50:52 +0800

    客製化部分加了第三層架構

---
+++ Customized_Shoes.dll/Models/Exts/ShoesSearchCriteria.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Customized_Shoes.dll.Models.Exts
+{
+	public class ShoesSearchCriteria
+	{
+		public string Name { get; set; }
+		public int? ShoesUnitPrice { get; set; }
+		public string Status { get; set; }
+		public List<string> StatusOption { get; set; }
+		public ShoesSearchCriteria()
+		{
+			StatusOption = new List<string>
+			{
+				"請選擇狀態",
+				"上架中",
+				"已下架"
+			};
+		}
+	}
+}
---
+++ Customized_Shoes.dll/Models/Interface/IShoesRepository.cs
@@ -0,0 +1,19 @@
+﻿using Customized_Shoes.dll.Models.Dtos;
+using Customized_Shoes.dll.Models.Exts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Customized_Shoes.dll.Models.Interface
+{
+	public interface IShoesRepository
+	{
+		void CreateShoes(CustomizedShoesDto dto);
+
+		List<CustomizedShoesDto> Search(ShoesSearchCriteria criteria);
+
+		void EditProductsStatus(List<CustomizedShoesDto> dto);
+	}
+}
