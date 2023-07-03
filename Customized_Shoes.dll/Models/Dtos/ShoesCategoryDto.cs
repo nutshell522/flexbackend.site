0fad59130fe6f83d4f718155661cf811bda5dd0c
Cheng Pin <as48957210@gmail.com>
2023-07-03 09:19:12 +0800

    寫客製化部分裡的商品

---
+++ Customized_Shoes.dll/Models/Dtos/ShoesCategoryDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Customized_Shoes.dll.Models.Dtos
+{
+	public class ShoesCategoryDto
+	{
+		public int ShoesCategoryId { get; set; }
+
+		public string ShoesCategoryName { get; set; }
+
+	}
+}
---
+++ Customized_Shoes.dll/Models/Exts/Result.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Customized_Shoes.dll.Models.Exts
+{
+	public class Result
+	{
+		public bool IsSucces { get; set; }
+		public bool IsFailed => !IsSucces;
+		public string ErroeMessage { get; set; }
+		public static Result Success() => new Result { IsSucces = true, ErroeMessage = null };
+
+		public static Result Fail(string errormessage) => new Result { IsSucces = false, ErroeMessage = errormessage };
+	}
+}
