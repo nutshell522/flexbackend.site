2a86138d307adb906984ec0fab92e3774c2c1081
Cheng Pin <as48957210@gmail.com>
2023-07-05 16:43:08 +0800

    分類管理寫完啦~

---
+++ Customized_Shoes.dll/Models/Exts/ShoesColorCategoryCriteria.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Customized_Shoes.dll.Models.Exts
+{
+	public class ShoesColorCategoryCriteria
+	{
+		public string ColorName { get; set; }
+
+		public string ColorCode { get; set; }
+	}
+}
--- flexbackend.site/Controllers/SuppliersController.cs
+++ flexbackend.site/Controllers/SuppliersController.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using Customized_Shoes.dll.Models.Exts;
 using Customized_Shoes.dll.Models.ViewModels;
 using EFModels.EFModels;
 
@@ -18,22 +19,24 @@ namespace flexbackend.site.Controllers
 		// GET: Suppliers
 		public ActionResult Index(SupplierCriteria criteria)
 		{
+			criteria = criteria ?? new SupplierCriteria();
+
 			ViewBag.Criteria = criteria;
 
-			var query = db.Suppliers;
+			IEnumerable<Supplier> query = db.Suppliers;
 
 			#region where
 			if (string.IsNullOrEmpty(criteria.CompanyName) == false)
 			{
-				query = (DbSet<Supplier>)query.Where(p => p.SupplierCompanyName.Contains(criteria.CompanyName));
+				query = query.Where(p => p.SupplierCompanyName.Contains(criteria.CompanyName));
 			}
 			if (criteria.Id != null && criteria.Id.Value > 0)
 			{
-				query = (DbSet<Supplier>)query.Where(p => p.SupplierId == criteria.Id.Value);
+				query = query.Where(p => p.SupplierId == criteria.Id.Value);
 			}
 			if (string.IsNullOrEmpty(criteria.Supply_Material) == false)
 			{
-				query = (DbSet<Supplier>)query.Where(p => p.Supply_Material.Contains(criteria.Supply_Material));
+				query = query.Where(p => p.Supply_Material.Contains(criteria.Supply_Material));
 			}
 
 			#endregion
